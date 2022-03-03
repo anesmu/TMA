575709dce53b04efc493fc0cd9734769e850b901
Antonio Espino Muñoz <antonioespinom@gmail.com>
2022-03-03 19:48:04 +0100

    git ignore files

--- Pods/Target Support Files/Pods-TMA-TMAUITests/Pods-TMA-TMAUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMA_TMAUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TMA_TMAUITests
-@end
--- Pods/Target Support Files/Pods-TMA/Pods-TMA-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMA : NSObject
-@end
-@implementation PodsDummy_Pods_TMA
-@end
